94bfdeb27a8fde9799f555fd170c0e072b31929d
Akshay <akshay.gp@drillbitplagiarism.com>
2023-09-25 11:08:55 +0530

    blocker issue resolved

--- src/app/components/form/elements/InputDatePicker.tsx
+++ src/app/components/form/elements/InputDatePicker.tsx
@@ -1,5 +1,5 @@
 import React, { KeyboardEvent } from 'react'
-import { Controller, FieldValues, Control } from 'react-hook-form'
+import { useController, FieldValues, Control } from 'react-hook-form'
 import { TextField, InputLabel, Stack } from '@mui/material'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import { DatePicker, LocalizationProvider } from '@mui/lab'
@@ -26,56 +26,58 @@ const InputDatePicker = ({ control, field }: InputDatePickerProps) => {
     e.preventDefault()
   }
 
+  const {
+    field: { onChange, value },
+    fieldState: { error },
+  } = useController({
+    name: field.name || '',
+    control,
+    rules: {
+      required: field.required,
+    },
+  })
+
   return (
     <>
       <InputLabel>{field.label}</InputLabel>
-      <Controller
-        name={field.name || ''}
-        control={control}
-        render={({ field: { onChange, value }, fieldState: { error } }) => (
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Stack spacing={3}>
-              <DatePicker
-                inputFormat="dd/MM/yyyy"
-                label={field.dateLabel}
-                value={value === undefined ? null : value}
-                onChange={onChange}
-                disableFuture={field.nextDate}
-                disablePast={field.prevDate}
-                minDate={field.minDate}
-                maxDate={field.maxDate}
-                renderInput={(params) => (
-                  <div className="form-element">
-                    <div className="datepicker">
-                      <TextField
-                        margin="normal"
-                        className="mt-5"
-                        onKeyDown={onKeyDown}
-                        value={
-                          params.inputProps?.value === undefined || ''
-                            ? null
-                            : params.inputProps?.value
-                        }
-                        fullWidth
-                        size="small"
-                        error={!!value}
-                        helperText={error ? field.message : field.info}
-                        {...params}
-                        inputProps={{
-                          className:'p-12',
-                        }}
-                      />
-                    </div>
-                  </div>
-                )}
-              />
-            </Stack>
-          </LocalizationProvider>
-        )}
-        rules={{
-          required: field.required,
-        }}
-      />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={3}>
+          <DatePicker
+            inputFormat="dd/MM/yyyy"
+            label={field.dateLabel}
+            value={!error ? (value === undefined ? null : value) : ''}
+            onChange={onChange}
+            disableFuture={field.nextDate}
+            disablePast={field.prevDate}
+            minDate={field.minDate}
+            maxDate={field.maxDate}
+            renderInput={(params) => (
+              <div className="form-element">
+                <div className="datepicker">
+                  <TextField
+                    margin="normal"
+                    className="mt-5"
+                    onKeyDown={onKeyDown}
+                    value={
+                      params.inputProps?.value === ''
+                        ? ''
+                        : params.inputProps?.value
+                    }
+                    fullWidth
+                    size="small"
+                    error={!!error}
+                    helperText={error ? field.message : field.info}
+                    {...params}
+                    inputProps={{
+                      className: 'p-12',
+                    }}
+                  />
+                </div>
+              </div>
+            )}
+          />
+        </Stack>
+      </LocalizationProvider>
     </>
   )
 }
